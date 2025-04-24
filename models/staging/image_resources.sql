5a870f84783e329e2044ebe8ad773bb6e678034d
Raphael Mengato <rmengato.da@gmail.com>
2025-04-24 02:42:28 +0000

    corrected bucket name

--- models/staging/image_resources.sql
+++ models/staging/image_resources.sql
@@ -6,6 +6,6 @@ SELECT
   primaryImage,
   primaryImageSmall,
   additionalImages,
-  concat('gs://qr_art_gallery_zoom_bucket/qr_codes/', objectID, '.png') as qrCodeBucketUri,
+  concat('qr_codes/', objectID, '.png') as qrCodeBucketUri,
   BatchTimestamp
 FROM {{ source('qr_art_gallery_raw', 'staging_metropolitan_images') }}
