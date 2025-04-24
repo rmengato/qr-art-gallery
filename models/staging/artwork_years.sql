3e866528d567bf82a472dca100fc7e7fcabd42d4
Raphael Mengato <rmengato.da@gmail.com>
2025-04-24 00:05:18 +0000

    specified only paintings in artworks (maybe provisory)

--- models/staging/artwork_years.sql
+++ models/staging/artwork_years.sql
@@ -8,16 +8,30 @@ with
             artistdisplayname,
             safe_cast(objectbegindate as int64) as object_start,
             safe_cast(objectenddate as int64) as object_end,
-            objectDate
+            objectDate,
+            classification,
+            culture,
+            department
         from {{ ref('staging_metropolitan_artworks') }}
         where
             safe_cast(objectbegindate as int64) is not null
             and safe_cast(objectenddate as int64) is not null
-            and ishighlight = true
-            and safe_cast(artistbegindate as int64) >= 1300
-            and not regexp_contains(objectdate, r'^\d{1,2}th century$')
+            --and safe_cast(artistbegindate as int64) >= 1300
+            --and not regexp_contains(objectdate, r'^\d{1,2}th century$')
+            and classification like '%aintings%'
+            and safe_cast(objectenddate as int64) - safe_cast(objectbegindate as int64) < 15
     )
 
-select objectid, title, artistdisplayname, object_start, object_end, year, objectdate
+select objectid, 
+    title, 
+    artistdisplayname, 
+    object_start, 
+    object_end, 
+    year, 
+    objectdate,
+    classification,
+    culture,
+    ifnull(culture, concat('not specified (', department, ')')) as culture_fallback,
+    department
 from artworks, unnest(generate_array(object_start, object_end)) as year
-order by objectid, year
+order by artistdisplayname, objectid, year
